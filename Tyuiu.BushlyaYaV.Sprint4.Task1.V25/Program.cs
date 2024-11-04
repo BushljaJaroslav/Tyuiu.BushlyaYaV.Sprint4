3dafe3658f24c400d231250db135c64105f6e05b
Tyuiu.BushljaJV@outlook.com <Tyuiu.BushljaJV@outlook.com>
2024-11-04 22:56:01 +0500

    Task1

---
+++ Tyuiu.BushlyaYaV.Sprint4.Task1.V25/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using Tyuiu.BushlyaYaV.Sprint4.Task1.V25.Lib;
+
+namespace Tyuiu.BushlyaYaV.Sprint4.Task1.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Бушля Я. В. | ИСТНб-24-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                *");
+            Console.WriteLine("* Тема: Одномерные массивы. (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание №1                                                               *");
+            Console.WriteLine("* Вариант #25                                                              *");
+            Console.WriteLine("* Выполнил: Бушля Ярослав Владимирович | ИСТНб-24-1                        *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                          *");
+            Console.WriteLine("****************************************************************************");
+
+            int len;
+            int sumArray;
+            Console.Write("Введите количество элементов массива: ");
+            len = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[len];
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write("Введите значение " + i + " элемента массива: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            sumArray = ds.Calculate(array);
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("Сумма элементов массива = " + sumArray);
+
+
+            Console.ReadKey();
+        }
+    }
+}
