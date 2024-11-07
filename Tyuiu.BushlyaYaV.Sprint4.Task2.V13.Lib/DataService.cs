5feaef41da042d7028ab490a42549d8bb8bd0f46
Tyuiu.BushljaJV@outlook.com <Tyuiu.BushljaJV@outlook.com>
2024-11-07 20:42:03 +0500

    Task2

---
+++ Tyuiu.BushlyaYaV.Sprint4.Task2.V13.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.BushlyaYaV.Sprint4.Task2.V13.Lib
+{
+    public class DataService : ISprint4Task2V13
+    {
+        public int Calculate(int[] array)
+        {
+            int p = 1;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if ((array[i] % 2 == 0) && array[i] >= 2 && array[i] <= 9)
+                {
+                    p *= array[i];
+                }
+            }
+            return p;
+        }
+    }
+}
---
+++ Tyuiu.BushlyaYaV.Sprint4.Task2.V13.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+
+using Tyuiu.BushlyaYaV.Sprint4.Task2.V13.Lib;
+
+namespace Tyuiu.BushlyaYaV.Sprint4.Task2.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Calculate()
+        {
+            DataService ds = new DataService();//
+            int[] numArray = { 2, 10, 8, 5, 4, 3, 2, 1, 6, 3, 4, 8, 7 };
+            int res = ds.Calculate(numArray);
+            Assert.AreEqual(24576, res);
+        }
+    }
+}
