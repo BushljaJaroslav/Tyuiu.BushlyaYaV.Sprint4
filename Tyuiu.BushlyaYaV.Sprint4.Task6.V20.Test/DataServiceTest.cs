fee25c3e9a87e03c38c1b7e5c6d61aa51045ab67
Tyuiu.BushljaJV@outlook.com <Tyuiu.BushljaJV@outlook.com>
2024-11-12 21:56:45 +0500

    Task6

---
+++ Tyuiu.BushlyaYaV.Sprint4.Task6.V20.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+
+using Tyuiu.BushlyaYaV.Sprint4.Task6.V20.Lib;
+
+namespace Tyuiu.BushlyaYaV.Sprint4.Task6.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string[] mas = { "Математика", "Физика", "Химия", "Биология", "География", "История", "Литература" };
+            string[] wait = { "Физика", "Химия", "Биология", "География", "История" };
+            string[] res = ds.Calculate(mas);
+            CollectionAssert.AreEqual(wait, res);
+
+        }
+    }
+}
